3347372ff068ddc5d2b9519965e643b20dc31cd6
Vasilios Magriplis <etameson@gmail.com>
2019-08-16 23:37:28 +0930

    Fix bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = (env, argv) => { 
+module.exports = (env, argv) => {
     let isDevelopment = (process.env.NODE_ENV = argv['mode']) !== 'production'
 
     return {
-
         entry: './src/index.tsx',
+
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'main.js'
+        },
 
         // Enable sourcemaps for debugging webpack's output.
         devtool: 'source-map',
@@ -38,45 +42,53 @@ module.exports = (env, argv) => {
                 {
                     test: /\.module\.s(a|c)ss$/,
                     loader: [
-                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        // As of css-loader 3, the options have changed
-                        // https://github.com/webpack-contrib/css-loader
-                        options: {
-                        modules: {
-                                localIdentName: '[folder]__[local]__[hash:base64:5]'
-                            },
-                            localsConvention: 'camelCase'
+                        isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            // As of css-loader 3, the options have changed
+                            // https://github.com/webpack-contrib/css-loader
+                            options: {
+                                modules: {
+                                    localIdentName: '[folder]__[local]__[hash:base64:5]'
+                                },
+                                localsConvention: 'camelCase'
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
                         }
-                    },
-                    {
-                        loader: 'sass-loader',
-                    }
                     ]
                 },
                 {
                     test: /\.scss$/,
                     exclude: /\.module.(s(a|c)ss)$/,
                     use: [
-                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader"
+                        isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "sass-loader"
                     ]
                 },
                 {
                     test: /\.(png|jpe?g|gif|svg)$/,
                     use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                        fallback: 'file-loader'
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                fallback: 'file-loader'
+                            }
                         }
-                    }
                     ]
                 }
             ]
         },
+
+        plugins: [
+            new MiniCssExtractPlugin({
+                // Options similar to the same options in webpackOptions.output
+                // both options are optional
+                filename: "main.css"
+            })
+        ],
 
         // When importing a module whose path matches one of the following, just
         // assume a corresponding global variable exists and use that instead.
