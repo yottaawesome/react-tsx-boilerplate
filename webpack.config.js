98d9b7862bfd98e49181b58ae9828afd2d420c3e
Vasilios Magriplis <etameson@gmail.com>
2019-10-01 22:37:54 +1000

    Update deps, simplify start script

--- webpack.config.js
+++ webpack.config.js
@@ -97,16 +97,5 @@ module.exports = (env, argv) => {
                 template: path.join(__dirname, './src/index.html')
             })
         ],
-
-        // When importing a module whose path matches one of the following, just
-        // assume a corresponding global variable exists and use that instead.
-        // This is important because it allows us to avoid bundling all of our
-        // dependencies, which allows browsers to cache those libraries between builds.
-        // These dependencies are imported at the index.html level.
-        externals: {
-            'react': 'React',
-            'react-dom': 'ReactDOM',
-            'react-router-dom': 'ReactRouterDOM'
-        }
     }
 }
